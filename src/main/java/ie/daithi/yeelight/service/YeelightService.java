2f03d814c825b3c5201f51e5126f0dea449dbedf
Hearn <dahearn@deloitte.ie>
2018-09-10 18:09:14 +0100

    Adding a delay

--- src/main/java/ie/daithi/yeelight/service/YeelightService.java
+++ src/main/java/ie/daithi/yeelight/service/YeelightService.java
@@ -1,5 +1,6 @@
 package ie.daithi.yeelight.service;
 
+import com.mollin.yapi.enumeration.YeelightEffect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.mollin.yapi.YeelightDevice;
@@ -10,17 +11,24 @@ public class YeelightService implements LightService {
 	@Value("${yeelight.url}")
 	private String yeelightUrl;
 
+	@Value("${yeelight.delay.in.millis}")
+	private int delayInMillis;
+
     @Override
     public void turnOn() throws Exception {
 
         // Instantiate your device (with its IP)
         YeelightDevice device = new YeelightDevice(yeelightUrl);
-        // Switch on the device
-        device.setPower(true);
         // Change device color
         device.setRGB(255, 180, 100);
         // Change device brightness
         device.setBrightness(100);
+		// Set effect
+		device.setEffect(YeelightEffect.SMOOTH);
+		// Set duration of effect
+		device.setDuration(delayInMillis);
+		// Switch on the device
+		device.setPower(true);
 
     }
 
@@ -29,6 +37,10 @@ public class YeelightService implements LightService {
 
 		// Instantiate your device (with its IP)
 		YeelightDevice device = new YeelightDevice(yeelightUrl);
+		// Set effect
+		device.setEffect(YeelightEffect.SMOOTH);
+		// Set duration of effect
+		device.setDuration(delayInMillis);
 		// Switch off the device
 		device.setPower(false);
 
