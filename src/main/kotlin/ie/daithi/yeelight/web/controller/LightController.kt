ffd3d46f6fc50255e06503c19e8d8dbb33d6ef39
Daithi Hearn <daithi.hearn@gmail.com>
2020-03-15 21:16:02 +0100

    Adding some details to the API definitions. Minor fixes/improvements.

--- src/main/kotlin/ie/daithi/yeelight/web/controller/LightController.kt
+++ src/main/kotlin/ie/daithi/yeelight/web/controller/LightController.kt
@@ -6,6 +6,10 @@ import ie.daithi.yeelight.enumeration.Event
 import ie.daithi.yeelight.model.PlexPayload
 import ie.daithi.yeelight.service.LightService
 import ie.daithi.yeelight.web.exceptions.BadGatewayException
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Value
@@ -17,21 +21,25 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @RestController
+@Api(tags = ["Yeelight"], description = "These endpoints perform operations on a Yeelight")
 class LightController (
     private val yeelightService: LightService,
     @Value("\${yeelight.url}") private val yeelightUrl: String,
-    @Value("#{'\${plex.supported.users}'.split(',')}")
-    private var supportedUsers: List<Long?>? = null,
-    @Value("#{'\${plex.supported.players}'.split(',')}") private val supportedPlayers: List<String>? = null,
-    @Value("#{'\${plex.supported.media}'.split(',')}") private var supportedMedia: MutableList<String?>? = null,
+    @Value("#{'\${plex.supported.users}'.split(',')}") private var supportedUsers: List<Long>,
+    @Value("#{'\${plex.supported.players}'.split(',')}") private val supportedPlayers: List<String>,
+    @Value("#{'\${plex.supported.media}'.split(',')}") private val supportedMedia: MutableList<String?>,
     private val objectMapper: ObjectMapper
 ) {
 
     @PostMapping("/toggle")
-    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @ResponseStatus(value = HttpStatus.OK)
     @Throws(BadGatewayException::class)
+    @ApiOperation(value = "Toggle", notes = "Toggles the light on and off")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Opertation was completed successfully"),
+            ApiResponse(code = 502, message = "An issue occurred when calling the Yeelight.")
+    )
     fun toggleLivingroomEndpoint() {
-
         try {
             yeelightService.toggle(yeelightUrl)
         } catch (ex : YeelightSocketException) {
@@ -40,8 +48,13 @@ class LightController (
     }
 
     @PostMapping("/turnoff")
-    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @ResponseStatus(value = HttpStatus.OK)
     @Throws(BadGatewayException::class)
+    @ApiOperation(value = "Turn Off", notes = "Turns the light off")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Opertation was completed successfully"),
+            ApiResponse(code = 502, message = "An issue occurred when calling the Yeelight.")
+    )
     fun turnOffLivingroomEndpoint() {
         try {
             yeelightService.turnOff(yeelightUrl)
@@ -51,8 +64,13 @@ class LightController (
     }
 
     @PostMapping("/turnon")
-    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @ResponseStatus(value = HttpStatus.OK)
     @Throws(BadGatewayException::class)
+    @ApiOperation(value = "Turn On", notes = "Turns the light on")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Opertation was completed successfully"),
+            ApiResponse(code = 502, message = "An issue occurred when calling the Yeelight.")
+    )
     fun turnOnLivingroomEndpoint(request: HttpServletRequest?, response: HttpServletResponse?) {
         try {
             yeelightService.turnOn(yeelightUrl)
@@ -63,22 +81,28 @@ class LightController (
 
 
     @PostMapping(value = ["/plexEndpoint"])
-    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @ResponseStatus(value = HttpStatus.OK)
     @Throws(BadGatewayException::class)
+    @ApiOperation(value = "Plex Endpoint", notes = "Listens for calls from a Plex Media Server web hook. It parses the " +
+            "payload and acts upon it. For the whitelisted players, media types and users: " +
+            "\\n \\t 1. On a PLAY or RESUME event the light will fade out " +
+            "\\n \\t 2. On a STOP or PAUSE event the light will fade in ")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Opertation was completed successfully"),
+            ApiResponse(code = 502, message = "An issue occurred when calling the Yeelight.")
+    )
     fun plexPostEndpoint(request: MultipartHttpServletRequest, @RequestParam("files") files: Array<MultipartFile?>) {
-        logger.info("Files: {}", files.size)
+
         val payloadString: String = request.getParameter("payload")
         val payload: PlexPayload = objectMapper.readValue(payloadString, PlexPayload::class.java)
-        logger.info("Payload: {}", payload)
+
         val event = Event.fromValue(payload.event)
-        if (event == null) {
-            logger.info("Event type '{}' not supported.", payload.event)
-        } else if (payload.account != null && !supportedUsers!!.contains(payload.account.id)) {
-            logger.info("User '{}' is not supported.", payload.account.id)
-        } else if (payload.player != null && !supportedPlayers!!.contains(payload.player.uuid)) {
-            logger.info("Player '{}' is not supported.", payload.player.uuid)
-        } else if (payload.metadata != null && !supportedMedia!!.contains(payload.metadata.type)) {
-            logger.info("Media type '{}' is not supported.", payload.metadata.type)
+        if (payload.account != null && !supportedUsers.contains(payload.account.id)) {
+            if (logger.isDebugEnabled()) logger.debug("User ${payload.account.id} is not supported.")
+        } else if (payload.player != null && !supportedPlayers.contains(payload.player.uuid)) {
+            if (logger.isDebugEnabled()) logger.debug("Player '${payload.player.uuid}' is not supported.")
+        } else if (payload.metadata != null && !supportedMedia.contains(payload.metadata.type)) {
+            if (logger.isDebugEnabled()) logger.debug("Media type '${payload.metadata.type}' is not supported.")
         } else if (Event.PLAY.equals(event) || Event.RESUME.equals(event)) {
             logger.info("Turning light off.")
             yeelightService.turnOffFade(yeelightUrl)
@@ -86,7 +110,7 @@ class LightController (
             logger.info("Turning light on.")
             yeelightService.turnOnFade(yeelightUrl)
         } else {
-            logger.info("None of the above.")
+            if (logger.isDebugEnabled()) logger.debug("None of the above.")
         }
     }
 
